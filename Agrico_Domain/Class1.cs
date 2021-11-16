70dae6331e4bbd29a8d8e11fe7c72e5fa274d6b1
Yodi34 <yohan34800@gmail.com>
2021-11-16 15:20:27 +0100

    d

--- Agrico_Domain/Class1.cs
+++ Agrico_Domain/Class1.cs
@@ -5,5 +5,6 @@
         public int MyProperty { get; set; }
         public int MyProperty2 { get; set; }
         public int MyProperty3 { get; set; }
+        public int MyProperty5 { get; set; }
     }
 }
